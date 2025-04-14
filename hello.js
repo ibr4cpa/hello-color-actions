d49f25a24c0f243c9a3c9a6b200af2d5eb8961b0
ibracpa <isabricpa@gmail.com>
2025-04-14 04:36:31 -0400

    hello again

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
 const hello = () => "Hello from Montreal from the Office Hours in the terminal!";
-console.log(hello());
+console.log(hello() + " 👋 Hello again from Montreal!");
 exports.hello = hello;
